4825b91d9fc4063af4b77f07741de7b222e17d12
Aleksandr-ava <167477534+Aleksandr-ava@users.noreply.github.com>
2024-08-18 15:59:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import inspect
+
+
+def introspection_info(obj):
+    print({'type': type(obj), 'attributes': dir(obj)})
+    function_module = inspect.getmodule(introspection_info)
+    print('module', type(function_module), function_module)
+    print('methods', inspect.getmembers(introspection_info))
+    return obj
+
+
+number_info = introspection_info
+print(number_info(42))
